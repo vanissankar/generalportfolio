15c54e6ddaca28ae78b081548364211c75d2afde
vanissankar <uarduino123@gmail.com>
2025-11-26 00:14:35 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('react-root') || document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
